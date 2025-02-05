07d7937abbfa390ca7cc981741ae07e595b2fdfd
M S I <egasanjaya2712@gmail.com>
2025-02-05 10:17:01 +0700

    lupa commit lagi pak, maaf

--- app/src/main/java/com/ega/laundry/LayananActivity.kt
+++ app/src/main/java/com/ega/laundry/LayananActivity.kt
@@ -6,11 +6,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class laundryisan : AppCompatActivity() {
+class LayananActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_laundryisan)
+        setContentView(R.layout.activity_layanan)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
